911f841db02dd9e91447e4eeb6a722353dfaacd1
Senu Gamer Boy <86920820+SenuGamerBoy@users.noreply.github.com>
2021-08-18 20:03:54 +0530

    Updated All Files To V1.1

---
+++ bot/plugins/commands.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @AlbertEinsteinTG
+
+from pyrogram import filters, Client
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from bot import Translation # pylint: disable=import-error
+from bot.database import Database # pylint: disable=import-error
+
+db = Database()
+
+@Client.on_message(filters.command(["start"]) & filters.private, group=1)
+async def start(bot, update):
+    
+    try:
+        file_uid = update.command[1]
+    except IndexError:
+        file_uid = False
+    
+    if file_uid:
+        file_id, file_name, file_caption, file_type = await db.get_file(file_uid)
+        
+        if (file_id or file_type) == None:
+            return
+        
+        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + "</code>")
+        
+        if file_type == "document":
+        
+            await bot.send_document(
+                chat_id=update.chat.id,
+                document = file_id,
+                caption = caption,
+                parse_mode="html",
+                reply_to_message_id=update.message_id,
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton
+                                (
+                                    'Developers', url="https://t.me/senuinfinity"
+                                )
+                        ]
+                    ]
+                )
+            )
+
+        elif file_type == "video":
+        
+            await bot.send_video(
+                chat_id=update.chat.id,
+                video = file_id,
+                caption = caption,
+                parse_mode="html",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton
+                                (
+                                    'Developers', url="https://t.me/senuinfinity"
+                                )
+                        ]
+                    ]
+                )
+            )
+            
+        elif file_type == "audio":
+        
+            await bot.send_audio(
+                chat_id=update.chat.id,
+                audio = file_id,
+                caption = caption,
+                parse_mode="html",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton
+                                (
+                                    'Developers', url="https://t.me/senuinfinity"
+                                )
+                        ]
+                    ]
+                )
+            )
+
+        else:
+            print(file_type)
+        
+        return
+
+    buttons = [[
+        InlineKeyboardButton('Developers', url='https://t.me/senuinfinity'),
+        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/SenuGamerBoy/Auto-Filter-v1')
+    ],[
+        InlineKeyboardButton('Support 🛠', url='https://t.me/senuinfinitygroup')
+    ],[
+        InlineKeyboardButton('Help ⚙', callback_data="help")
+    ]]
+    
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await update.reply_sticker(sticker="CAACAgIAAx0CSRAc2gACJNBhHRevzV5PCj7n2EhqV6paTPWcAwACvwMAAvJ-ggw0tGb0ymY7IyAE")
+
+
+@Client.on_message(filters.command(["help"]) & filters.private, group=1)
+async def help(bot, update):
+    buttons = [[
+        InlineKeyboardButton('Home ⚡', callback_data='start'),
+        InlineKeyboardButton('About 🚩', callback_data='about')
+    ],[
+        InlineKeyboardButton('Close 🔐', callback_data='close')
+    ]]
+    
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.HELP_TEXT,
+        reply_markup=reply_markup,
+        parse_mode="html",
+        reply_to_message_id=update.message_id
+    )
+
+
+@Client.on_message(filters.command(["about"]) & filters.private, group=1)
+async def about(bot, update):
+    
+    buttons = [[
+        InlineKeyboardButton('Home ⚡', callback_data='start'),
+        InlineKeyboardButton('Close 🔐', callback_data='close')
+    ]]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await update.reply_sticker(sticker="CAACAgIAAx0CSRAc2gACJNBhHRevzV5PCj7n2EhqV6paTPWcAwACvwMAAvJ-ggw0tGb0ymY7IyAE")
