2fa7223eecdecaf84e4e25add5477a9ad6134d23
oshan618 <78407695+oshan618@users.noreply.github.com>
2021-09-24 10:53:31 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -11,7 +11,7 @@ Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥�
 
 For More Details Click Help Button Below..
 
-@SenuGamerBoy
+@Kevin00618
 </i>"""    
     
     HELP_TEXT = """
@@ -44,10 +44,10 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@SenuGamerBoy
+@Kevin00618
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : @SenuGamerBoy
+<b>➥ Creator</b> : @Kevin00618
 """
